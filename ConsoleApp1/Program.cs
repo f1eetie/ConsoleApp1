19674d9c784ebbddb3bdf6b175df71ff023186df
Martin Poyser <fleetie@fleetie.co.uk>
2017-06-02 15:57:49 +0100

    Commit 6

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,7 +9,7 @@ using Microsoft.VisualBasic; // For .FileIO.TextFieldParser
 using System.Xml;
 using System.Text.RegularExpressions;
 
-// Here is ANOTHER 5 comment added on the GitHub website
+// Here is ANOTHER 6 comment added on the GitHub website
 
 namespace ConsoleApp1
 {
